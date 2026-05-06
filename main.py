c946ac03bc472ff135874270da738fd79a362cfb
craigt2005 <CraigT@Electroimpact.com>
2026-05-06 19:14:38 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from artnetListener import ArtnetListener
+from machine import Pin
+import neopixel
+import time
+
+
+
+def packet_handler(packet):
+    if packet["universe"] == 3:
+        for i in range(0,14):
+            np[i] = (packet["channels"][i*3+offset],packet["channels"][i*3+1+offset],packet["channels"][i*3+2+offset])
+    
+
+    np.write()
+    #print(f"Packet recieved: {packet}")
+    
+def connection_state_changed_handler(state):
+    print(f"New State: {state}")
+
+np = neopixel.NeoPixel(Pin(0),14)
+
+offset = 4
+
+artnet = ArtnetListener()
+artnet.on_packet(packet_handler)
+artnet.listen()
